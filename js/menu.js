4f74934bfb42f180372b60e04d80486a937d59e3
Kenan Philips <philipskenan@gmail.com>
2017-03-13 21:51:05 -0700

    update

--- js/menu.js
+++ js/menu.js
@@ -20,23 +20,19 @@ $(document).ready(function() {
     });
   });
 
-  // $(function() {
-  //   if (screen.width < 800) {
-  //     $(".navbar-default").css("background-color", "#fff");
-  //     $(".navbar-default .navbar-nav>li>a").css("color", "#2e2e2e");
-  //   } else {
-  //     $(window).scroll(function() {
-  //       if ($(document).scrollTop() > 150) {
-  //         $(".navbar-default").css("background-color", "#fff");
-  //         $(".navbar-default").css("border-bottom", "1px solid #E0E0E0");
-  //         $(".navbar-default .navbar-nav>li>a").css("color", "#2e2e2e");
-  //       } else {
-  //         $(".navbar-default").css("background-color", "transparent");
-  //         $(".navbar-default").css("border-bottom", "none");
-  //         $(".navbar-default .navbar-nav>li>a").css("color", "#fff");
-  //       }
-  //     });
-  //   }
-  // });
+  $(function() {
+    if (screen.width < 800) {
+      // $(".navbar-default").css("border-radius", "1px solid #E0E0E0");
+      // $(".navbar-default .navbar-nav>li>a").css("color", "#2e2e2e");
+    } else {
+      $(window).scroll(function() {
+        if ($(document).scrollTop() > 150) {
+          $(".navbar-default").addClass("nav-shadow")
+        } else {
+          $(".navbar-default").removeClass("nav-shadow");
+        }
+      });
+    }
+  });
 
 });
